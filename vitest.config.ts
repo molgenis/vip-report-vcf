4204eacd1b5063a1145ca919be16563cc629f6d7
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-05-19 08:06:41 +0200

    refactor:check coverage for all files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      all: true,
       lines: 90,
       functions: 90,
       branches: 90,
