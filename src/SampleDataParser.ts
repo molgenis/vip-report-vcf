66bd98a6bfa10743768ad9fa50bc54d63ea1ab12
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-10-24 09:52:41 +0200

    chore:add docs, improve types

--- src/SampleDataParser.ts
+++ src/SampleDataParser.ts
@@ -48,13 +48,18 @@ export function parseFormatValue(token: string, formatMetadata: FieldMetadata): 
   return value;
 }
 
-export function calculateAlleleBalance(allelicDepthParam: (number | null)[]): ValueInteger {
-  if (allelicDepthParam.includes(null)) {
+/**
+ * Calculate allele balance: allele depth of first allele divided by total allele depth
+ *
+ * @param allelicDepths allele depth values with each value an integer or null
+ * @return allele balance or null if 1) allele depth array contains a null value 2) total allele depth is zero
+ */
+export function calculateAlleleBalance(allelicDepths: ValueInteger[]): ValueInteger {
+  if (allelicDepths.includes(null)) {
     return null;
   }
-  const allelicDepth: number[] = allelicDepthParam as number[];
-  const total = allelicDepth.reduce((x, y) => x + y);
-  return total != 0 ? allelicDepth[0] / total : null;
+  const total = (allelicDepths as number[]).reduce((x, y) => x + y);
+  return total != 0 ? (allelicDepths as number[])[0] / total : null;
 }
 
 export function parseGenotype(token: string): Genotype {
