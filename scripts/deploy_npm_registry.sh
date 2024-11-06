906d6c251b378fe2cef499cb2d6efaff1a31e5ec
dennishendriksen <d.hendriksen@umcg.nl>
2024-11-06 10:11:42 +0100

    update debug statement to fix travis deploy issue

--- scripts/deploy_npm_registry.sh
+++ scripts/deploy_npm_registry.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git status # debug
+git diff # debug
 pnpm config set registry "https://registry.npmjs.org"
 pnpm config set //registry.npmjs.org/:_authToken "${NPM_TOKEN}"
 pnpm publish .
