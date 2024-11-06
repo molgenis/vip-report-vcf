9c7f6efd1dffd193966671c91c476fe9ed205a18
dennishendriksen <d.hendriksen@umcg.nl>
2024-11-06 10:27:06 +0100

    add --no-git-checks to fix travis deploy issue, remove debug statement

--- scripts/deploy_npm_registry.sh
+++ scripts/deploy_npm_registry.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git diff # debug
 pnpm config set registry "https://registry.npmjs.org"
 pnpm config set //registry.npmjs.org/:_authToken "${NPM_TOKEN}"
-pnpm publish .
+# workaround: packageManager is added to package.json after git checkout resulting in 'Unclean working tree' error
+pnpm publish --no-git-checks .
